ea8fc83f797a3d4ed7c5551480ae2b68c1441a42
apoth <anna@poth-kandel.de>
2017-01-19 13:50:13 +0100

    change port
    Signed-off-by: apoth <anna@poth-kandel.de>

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ app.get('*', function(req, res) {
 	res.send("Incorrect route");
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 	console.log("listening");
 });
